a457d5897aa4b5ee429e52d437f86def3969b686
JORGE CASTRO GUTIÉRREZ <jorgecas71@MacBook-Air-de-JORGE.local>
2024-09-06 18:47:41 +0200

    added class moto

--- src/main/java/dev_jorge/Moto.java
+++ src/main/java/dev_jorge/Moto.java
@@ -1,5 +1,17 @@
 package dev_jorge;
 
-public class Moto {
+public class Moto extends Vehiculo {
+    public Moto(String placa) {
+        super(placa);
+    }
 
+    @Override
+    public double calcularPeaje() {
+        return 50;
+    }
+
+    @Override
+    public String toString() {
+        return "Moto [placa=" + placa + "]";
+    }
 }
