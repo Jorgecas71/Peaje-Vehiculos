70c0764e4b2e5f515bbecb4a1213af2bc3b4aac3
JORGE CASTRO GUTIÉRREZ <jorgecas71@MacBook-Air-de-JORGE.local>
2024-09-06 19:16:22 +0200

    added class camion and moto

--- src/main/java/dev_jorge/Camion.java
+++ src/main/java/dev_jorge/Camion.java
@@ -1,5 +1,20 @@
 package dev_jorge;
 
-public class Camion {
+public class Camion extends Vehiculo {
+    private int ejes;
 
+    public Camion(String placa, int ejes) {
+        super(placa);
+        this.ejes = ejes;
+    }
+
+    @Override
+    public double calcularPeaje() {
+        return 50 * ejes;
+    }
+
+    @Override
+    public String toString() {
+        return "Camion [placa=" + placa + ", ejes=" + ejes + "]";
+    }
 }
---
+++ src/main/java/dev_jorge/Coche.java
@@ -0,0 +1,18 @@
+package dev_jorge;
+
+
+public class Coche extends Vehiculo {
+    public Coche(String placa) {
+        super(placa);
+    }
+
+    @Override
+    public double calcularPeaje() {
+        return 100;
+    }
+
+    @Override
+    public String toString() {
+        return "Coche [placa=" + placa + "]";
+    }
+}
